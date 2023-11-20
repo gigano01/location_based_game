aedf17efa77ac252f6531d5b991ac21abfdc18df
gigano01 <noahvanleemput@hotmail.com>
2023-11-20 15:02:06 +0100

    made secret available for rest

---
+++ scripts/secrets.js
@@ -0,0 +1,4 @@
+// rename this file to secrets.js
+// and enter your public access token
+// from your account on https://account.mapbox.com
+const mapboxAccessToken = 'pk.eyJ1Ijoibm9haHZhbmxlZW1wdXQiLCJhIjoiY2xvOHNiaDJiMDR0bTJpcXBsdXVkaGV0cCJ9.wqMC3dc0pQUWP6__XuLtXw';

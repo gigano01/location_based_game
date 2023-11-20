2de6a1e5f7c4ca63f8812bb912d0fe047a6dc7b5
gigano01 <noahvanleemput@hotmail.com>
2023-11-20 14:10:38 +0100

    veranderd naar tabs

--- scripts/movement.js
+++ scripts/movement.js
@@ -2,21 +2,21 @@ var __MOUSEX
 var __MOUSEY
 
 function getMTPosition(){
-    return {__MOUSEX, __MOUSEY}
+	return {__MOUSEX, __MOUSEY}
 }
 
 //javascript is heel raar met dit soort dingen, maar dit is hoe je het doet bewegen.
 document.addEventListener('touchmove', function(e) {
-    e.preventDefault()
-    let touch = e.touches[0]
-    __MOUSEX = touch.pageX
-    __MOUSEY = touch.pageY
+	e.preventDefault()
+	let touch = e.touches[0]
+	__MOUSEX = touch.pageX
+	__MOUSEY = touch.pageY
 }, false)
 
 //en dit is voor het op de computer te laten werken.
 document.addEventListener('mousemove', (e) => {
-    //console.log(`mouse position: ${event.x}:${event.y}`);
+	//console.log(`mouse position: ${event.x}:${event.y}`);
 
-    vierkant.style.left = e.x
-    vierkant.style.top = e.y
+	vierkant.style.left = e.x
+	vierkant.style.top = e.y
 })

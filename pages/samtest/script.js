3d2d27aa774ddaa7a5b5f279ef3ffbbfe95933ff
Sammia <sammiadergent@hotmail.com>
2023-11-27 15:55:19 +0100

    locaties toegevoegd

--- pages/samtest/script.js
+++
@@ -1,6 +0,0 @@
-const startButton = document.querySelector('#start-button');
-
-startButton.onclick = () => {
-	const locationID = "test"
-	location.assign(`../navigate/index.html?locationID=${locationID}`)
-}

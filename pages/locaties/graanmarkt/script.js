b1b38f6782641020c9fb931f580768bcee8be2f9
gigano01 <noahvanleemput@hotmail.com>
2023-12-02 16:37:27 +0100

    botasnische tuin

--- pages/locaties/graanmarkt/script.js
+++ pages/locaties/graanmarkt/script.js
@@ -1,6 +1,20 @@
-const startButton = document.querySelector('#start-button');
+onScreen(1, ()=>{
+	setTimeout(nextScreen,3000)
+})
 
-startButton.onclick = () => {
-	const locationID = "test"
-	location.assign(`../navigate/index.html?locationID=${locationID}`)
-}
+onScreen(2, ()=>{
+	setTimeout(nextScreen,800)
+})
+
+onScreen(3, ()=>{
+	createDialogueObject("dialogue/scherm3.json").then((dialogue)=>{
+		assignDialogueToContainer(dialogue,document.getElementById("eekhoorntjetekst-s3"))
+		setDialogueEndHandler(dialogue,()=>{
+			nextScreen()
+		})
+	})
+})
+
+docReady(async ()=>{
+	gotoScreen(1)
+})

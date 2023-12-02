8f60749b26a34001b756a6445e9e1ec6191d882d
gigano01 <noahvanleemput@hotmail.com>
2023-12-02 23:34:39 +0100

    heb al veel gedaan

--- pages/locaties/meir/script.js
+++ pages/locaties/meir/script.js
@@ -1,6 +1,30 @@
-const startButton = document.querySelector('#start-button');
+onScreen(1,()=>{
+	setTimeout(nextScreen, 1000)
+})
 
-startButton.onclick = () => {
-	const locationID = "test"
-	location.assign(`../navigate/index.html?locationID=${locationID}`)
-}
+onScreen(2, ()=>{
+	createDialogueObject("dialogue/scherm2.json").then((dialogue)=>{
+		assignDialogueToContainer(dialogue,document.getElementById("muisjetekst-s2"))
+		setDialogueEndHandler(dialogue,()=>{
+			nextScreen()
+		})
+	})
+})
+
+onScreen(3,()=>{
+	setTimeout(nextScreen, 1000)
+})
+
+onScreen(4, ()=>{
+	createDialogueObject("dialogue/scherm4.json").then((dialogue)=>{
+		assignDialogueToContainer(dialogue,document.getElementById("muisjetekst-s4"))
+		setDialogueEndHandler(dialogue,()=>{
+			const nextlocID = ""
+			location.assign(`../../navigate/index.html?locationID=${nextlocID}`)
+		})
+	})
+})
+
+docReady(async ()=>{
+	gotoScreen(4)
+})

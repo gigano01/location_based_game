75098a243e3936d611a529876760573ccf3eb242
gigano01 <noahvanleemput@hotmail.com>
2023-12-22 15:23:22 +0100

    yes

--- pages/kranen/kluis/script.js
+++ pages/kranen/kluis/script.js
@@ -199,7 +199,7 @@ onScreen(12, ()=>{
 		//console.log(rotation)
 		img.style.transform = 'rotate(' + rotation + 'deg)'
 
-		if(rotation > 800) {
+		if(rotation > 800 || rotation < -800) {
 			onDragHandler = () => {}
 			nextScreen()
 		}

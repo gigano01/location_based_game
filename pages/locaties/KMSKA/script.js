8cbc0ce9b214258a6f597ddad2e2b0da556559dc
DeclanVB <declan.vanbeethoven@student.ap.be>
2024-01-05 16:43:49 +0100

    Bena klaar met Animaties

--- pages/locaties/KMSKA/script.js
+++ pages/locaties/KMSKA/script.js
@@ -2,9 +2,9 @@
 	setTimeout(nextScreen,3000)
 })
 
-// onScreen(2, ()=>{
-// 	setTimeout(nextScreen,800)
-// })
+onScreen(2, ()=>{
+	setTimeout(nextScreen, 2200)
+})
 
 onScreen(3, ()=>{
 	createDialogueObject("dialogue/scherm3.json").then((dialogue)=>{
